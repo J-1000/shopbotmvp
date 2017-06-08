cddc0c4babc6f99ce3718e4b414534d00edd063c
Jan Redmann <janredmann1@gmail.com>
2017-06-08 11:25:53 +0200

    Add delete list in routes

--- routes/botman.php
+++ routes/botman.php
@@ -1,6 +1,8 @@
 <?php
 use App\Conversations\AddItemsConversation;
+use App\Conversations\ExampleConversation;
 use App\Http\Controllers\BotManController;
+use App\Listing;
 
 $botman = resolve('botman');
 
@@ -8,12 +10,21 @@ $botman->hears('/start', function ($bot) {
     $bot->reply('Hallo ' . $bot->getUser()->getFirstName() .  '!! Gib die Artikel mit vorangestelltem + ein. Um die Liste anzuzeigen, gib das Kommando /liste ein');
 });
 
-$botman->hears('s', BotManController::class.'@startConversation');
+$botman->hears('s', function ($bot) {
+    $bot->startConversation(new ExampleConversation());
+});
 
-$botman->hears('/liste', BotManController::class. '@showListConversation');
+$botman->hears('/Liste', BotManController::class. '@showListConversation');
 
 $botman->hears("\\+ {item}", function ($bot, $item) {
     $bot->startConversation(new AddItemsConversation($item));
 });
 
 $botman->hears('/newlist', BotmanController::class. '@newListingConversation');
+
+$botman->hears('/Liste löschen', function ($bot) {
+    $listing = Listing::first();
+    $listing->delete();
+    $bot->reply('Die Liste wurde gelöscht');
+});
+
