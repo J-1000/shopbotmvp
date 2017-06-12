13830e343756d44a62890a8662b8f0d730b2c16a
Jan Redmann <janredmann1@gmail.com>
2017-06-12 16:16:42 +0200

    Add help command. Some cleanup.

--- routes/botman.php
+++ routes/botman.php
@@ -8,21 +8,20 @@ use App\Item;
 $botman = resolve('botman');
 
 $botman->hears('/start', function ($bot) {
-    $bot->reply('Hallo ' . $bot->getUser()->getFirstName() .  '!! Gib die Artikel mit vorangestelltem + ein. Um die Liste anzuzeigen, gib das Kommando /liste ein');
+    $bot->reply('Hallo ' .
+        $bot->getUser()->getFirstName() .
+        '!! Gib die Artikel mit vorangestelltem + ein. Um die Liste anzuzeigen, gib das Kommando /liste ein. 
+        Falls Du Hilfe brauchst -> /hilfe'
+    );
 });
 
-$botman->hears('s', function ($bot) {
-    $bot->startConversation(new ExampleConversation());
-});
-
-$botman->hears('/Liste', BotManController::class. '@showListConversation');
+$botman->hears('/liste', BotManController::class. '@showListConversation');
 
 $botman->hears("\\+ {item}", function ($bot, $item) {
     $bot->startConversation(new AddItemsConversation($item));
 });
 
 $botman->hears("\\- {item}", function ($bot, $item) {
-
     $itemModel = Item::where('name', $item)->first();
     $listing = Listing::first();
     $itemModel->listing()->dissociate($listing);
@@ -30,12 +29,21 @@ $botman->hears("\\- {item}", function ($bot, $item) {
     $bot->reply($item . ' wurde von der Liste entfernt.');
 });
 
-$botman->hears('/newlist', BotmanController::class. '@newListingConversation');
+//$botman->hears('/neue liste', BotmanController::class. '@newListingConversation');
 
-$botman->hears('/Liste löschen', function ($bot) {
+$botman->hears('/liste löschen', function ($bot) {
     $listing = Listing::first();
     $listing->delete();
     $bot->reply('Die Liste wurde gelöscht');
 });
 
-
+$botman->hears('/hilfe', function ($bot) {
+    $bot->typesAndWaits(1);
+    $bot->reply('Um einen Artikel der Liste hinzuzufügen, gib den Artikel mit vorangestelltem + ein.');
+    $bot->typesAndWaits(1);
+    $bot->reply('Ein vorangestelltes - entfernt den Artikel von der Liste');
+    $bot->typesAndWaits(1);
+    $bot->reply('Ein vorangestelltes - entfernt den Artikel von der Liste');
+    $bot->typesAndWaits(1);
+    $bot->reply('Gib / ein um eine Liste der Kommandos zu sehen.');
+});
